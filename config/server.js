d4d3dfbd155b1d4aba3b788564f3167b72e75144
RgarciaDD <rodri.garcia.gomez@gmail.com>
2021-08-24 20:44:45 +0100

    testBuild

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("APP_URL"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '291538458cbc59b80535a52f96086518'),
+      secret: env("ADMIN_JWT_SECRET", "291538458cbc59b80535a52f96086518"),
     },
   },
 });
